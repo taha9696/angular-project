4680e1bad2281a90afa82a8cec916f418d7c6305
Taha Bellotef <125126368+taha9696@users.noreply.github.com>
2025-02-06 19:57:15 +0100

    Update highlight.directive.spec.ts

--- src/app/directives/highlight.directive.spec.ts
+++ src/app/directives/highlight.directive.spec.ts
@@ -1,8 +1,23 @@
 import { HighlightDirective } from './highlight.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 
 describe('HighlightDirective', () => {
+  let directive: HighlightDirective;
+  let el: ElementRef;
+  let renderer: Renderer2;
+
+  beforeEach(() => {
+    // Create mock instances of ElementRef and Renderer2
+    el = {} as ElementRef;
+    renderer = {} as Renderer2;
+
+    // Create the directive with the mocked dependencies
+    directive = new HighlightDirective(el, renderer);
+  });
+
   it('should create an instance', () => {
-    const directive = new HighlightDirective();
     expect(directive).toBeTruthy();
   });
 });
+
